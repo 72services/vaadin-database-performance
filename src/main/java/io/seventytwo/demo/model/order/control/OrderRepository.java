ca570f0e1ce860b7a75f9b65066e34a453509885
Simon Martinelli <simon@72.services>
2022-01-03 15:56:04 +0100

    removed superfluous method

--- src/main/java/io/seventytwo/demo/model/order/control/OrderRepository.java
+++ src/main/java/io/seventytwo/demo/model/order/control/OrderRepository.java
@@ -10,21 +10,4 @@ public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     @Query("select c.orders from Customer c where c.id = :customerId")
     List<Order> findByCustomerId(Integer customerId);
-
-    @Query(value = """
-            select cast(pos as varchar)
-                from (select json_agg(po) as orders
-                     from (select p.id,
-                                  p.order_date,
-                                  (select json_agg(oi)
-                                   from (select i.id, i.quantity, pr.name as product
-                                         from order_item as i
-                                                  join product pr on pr.id = i.product_id
-                                         where i.order_id = p.id
-                                        ) oi
-                                  ) as items
-                           from purchase_order as p
-                           where p.customer_id = ?) po) pos
-            """, nativeQuery = true)
-    String findByCustomerIdAsJson(Integer customerId);
 }
